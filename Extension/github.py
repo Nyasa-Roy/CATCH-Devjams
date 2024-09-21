924380cd54ab7e270c2ec829f85b29d065ef2d1c
Mikul098 <mikulbenara7@gmail.com>
2024-09-21 10:19:18 +0530

    Update github.py
    changed the github file

--- Extension/github.py
+++ Extension/github.py
@@ -5,9 +5,13 @@ HEADER = {'Accept': 'application/vnd.github.v3.star+json'}
 
 for i in range(1, 19):
     url = f"https://api.github.com/users/{USERNAME}/starred?per_page=100&page={i}"
-    fname = f"ghstars-{USERNAME}-{i}.json"
-    
+    fname = f"GithubStarredpost-{USERNAME}-{i}.json"
     response = requests.get(url, headers=HEADER)
+    if response.status_code==200:
+         starredcontent=response.json()
+
+    if not starredcontent:
+        break
     if response.status_code == 200:
         with open(fname, 'w') as file:
             file.write(response.text)
